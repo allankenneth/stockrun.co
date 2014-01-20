05b5f5d1ae711767c7cbb1a5cfee57d3ffa11d49
allankh <hello@allankenneth.com>
2014-01-20 13:07:46 -0800

    Some formatting on index, but mainly this is all about shiptrack.php, a new feature that imports canada post customer detail reports and formats them for easy search and one-click tracking

---
+++ shiptrack.php
@@ -0,0 +1,115 @@
+<?php
+
+function getShipments() {
+	$f = fopen("files/Customer-Detail-Report.csv", "r");
+	$shipments = Array();
+	while (($line = fgetcsv($f)) !== false) {
+        $count = 0;
+        foreach ($line as $cell) {
+			if($count > 0) {
+				$itembit = htmlspecialchars($cell);
+				if($count == 3) $date = $itembit;
+				$dateformed = new DateTime($date);
+				$dateformed = $dateformed->format('M jS');
+				if($count == 4) $track = $itembit;
+				if($count == 5) $client = $itembit;
+				if($count == 8) $sku = $itembit;
+				if($count == 9) $type = $itembit;
+				if($count == 10) $weight = $itembit;
+				if($count == 17) $cost = $itembit;
+			}
+			$count++; //go up one each loop
+        }
+		array_push($shipments,[$dateformed,$client,$track,$sku,$type,$weight,$cost]);
+	}
+	fclose($f);
+	return $shipments;
+}
+function displayShipments($shipments) {
+	$shipments = array_reverse($shipments);
+	$ship = '';
+	$canpost ='http://www.canadapost.ca/cpotools/apps/track/personal/findByTrackNumber?trackingNumber=';
+	foreach($shipments as $shipment) {
+		$ship .= '<tr>';
+		$ship .= '<td class="date">'.$shipment[0].'</td>';
+		$ship .= '<td class="client"><strong>'.$shipment[1].'</strong></td>';
+		$url = $canpost . $shipment[2];
+		$ship .= '<td class="track"><a target="_blank" href="'.$url.'">'.$shipment[2].'</a></td>';
+		$ship .= '<td class="transaction">'.$shipment[3].'</td>';
+		$ship .= '<td class="ship-type">'.$shipment[4].'</td>';
+		$ship .= '<td class="ship-weight">'.$shipment[5].'kg</td>';
+		$ship .= '<td class="ship-cost">$'.$shipment[6].'</td>';
+		$ship .= '</tr>';
+	}
+	print $ship;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Store 5 - Shipments</title>
+	<link rel="apple-touch-icon" sizes="120x120" href="rabbit.png">
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/respond.js"></script>
+
+</head>
+<body id="body">
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container">
+	<div class="buttons">
+		
+	</div>
+  </div>
+</div>
+	
+<div id="wrap">
+	<div class="top container">
+		<div class="row">
+			<div class="col-12 col-lg-12">
+				<div id="shipments">
+					<form role="form">
+					  <div class="form-group">
+						  <input type="input" class="search" placeholder="Search">
+					  </div>
+					</form>
+					<table class="table">
+						<thead>
+						<tr>
+							<th class="sort" data-sort="date">Date</th>
+							<th class="sort" data-sort="ship-client">Client</th>
+							<th class="sort" data-sort="tracking">Tracking #</th>
+							<th class="sort" data-sort="transaction">Transaction ID</th>
+							<th class="sort" data-sort="ship-type">Shipping Type</th>
+							<th class="sort" data-sort="ship-weight">Weight</th>
+							<th class="sort" data-sort="name">Cost</th>
+						</tr>
+						</thead>
+						<tbody class="list">
+						<?php $shipments = getShipments() ?>
+						<?php displayShipments($shipments) ?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<script src="js/jquery-1.4.2.min.js"></script>
+<script src="js/list.min.js"></script>
+<!-- <script src="js/jquery.scrollTo.js"></script> -->
+<script>
+$(function() {
+	$(".search").focus();
+	var options = {
+		valueNames: [ 'date', 'client','track','transaction','ship-type','ship-weight','ship-cost']
+	};
+	var shipmentList = new List('shipments', options);
+});
+</script>
+</body>
+</html>
